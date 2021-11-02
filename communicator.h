da8ad97e86a8f82858082c5550be19d9c6856cba
nikita <myasov1992@list.ru>
2021-11-02 21:44:51 +0300

    Добавлены комменты

--- communicator.h
+++ communicator.h
@@ -1,7 +1,26 @@
+/**
+ * @file
+ * @brief файл класса Communicator
+ * @page Communicator -
+*/
 #ifndef COMMUNICATOR_H
 #define COMMUNICATOR_H
-
+#include <qstring.h>
 #include <QObject>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+
+/**
+ * @brief Класс для доступа к внешнему API
+ * @details Данный класс реализует доступ к веб-сервису для получения данных
+ * @authors МИРЭА
+ * @version 0.1
+ * @date 31.10.2021
+ * @copyright GPL 3.0
+ * @todo Реализовать запросы на все типы данных
+ * @warning Данный класс создан в учебных целях
+*/
 
 class Communicator : public QObject
 {
